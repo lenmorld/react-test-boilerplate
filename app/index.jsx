c952c90fffcc75395dbcbe1024fb2ba57d6d0742
Lenmor Larroza Dimanalata <lenmor.larroza.dimanalata@autodesk.com>
2019-07-12 16:16:08 -0400

    1_JEST_AND_BABEL

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Home from "./Home";
+
+class App extends React.Component {
+	render() {
+		return <Home></Home>;
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
